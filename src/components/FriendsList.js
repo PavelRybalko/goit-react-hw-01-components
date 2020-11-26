f28b827125072f31395047cdda5570c362da3911
PavelRybalko <freedom277f@gmail.com>
2020-11-26 17:32:05 +0200

    add lint,husky etc

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,17 +1,16 @@
-import FriendsListItem from "./FriendsListItem";
+import FriendsListItem from './FriendsListItem';
 
-export default function FriendsList({friends}){
-	return (
-		<ul class="friend-list">
-			{friends.map(el=>(
-				<FriendsListItem 
-					id={el.id}
-					avatar={el.avatar}
-					name={el.name}
-					isOnline={el.isOnline}
-				/>
-				)
-			)}
-		</ul>
-		)
+export default function FriendsList({ friends }) {
+  return (
+    <ul className="friend-list">
+      {friends.map(el => (
+        <FriendsListItem
+          key={el.id}
+          avatar={el.avatar}
+          name={el.name}
+          isOnline={el.isOnline}
+        />
+      ))}
+    </ul>
+  );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,44 +1,52 @@
 import PropTypes from 'prop-types';
 // import defaultImage from './default.jpg';
 
-export default function Profile({imageUrl, name, tag, location, followers, views, likes}) {
-	return (
-<div class="profile">
-	<div class="description">
-	    <img
-	      src={imageUrl}
-	      alt="Аватар пользователя"
-	      class="avatar"
-	      width="480"
-	    />
-	    <p class="name">{name}</p>
-	    <p class="tag">@{tag}</p>
-	    <p class="location">{location}</p>
-	</div>
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">{followers}</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">{views}</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">{likes}</span>
-    </li>
-  </ul>
-</div>
-	);
-};
+export default function Profile({
+  imageUrl,
+  name,
+  tag,
+  location,
+  followers,
+  views,
+  likes,
+}) {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+          src={imageUrl}
+          alt="Аватар пользователя"
+          className="avatar"
+          width="480"
+        />
+        <p className="name">{name}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 Profile.propTypes = {
-	imageUrl: PropTypes.string.isRequired,
-	name: PropTypes.string.isRequired,
-	tag: PropTypes.string.isRequired,
-	location: PropTypes.string.isRequired,
-	followers: PropTypes.number.isRequired,
-	views: PropTypes.number.isRequired,
-	likes: PropTypes.number.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
 };
